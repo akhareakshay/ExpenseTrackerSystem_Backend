d5893f0dcc23e71484f6aa19d2722261945b2bd5
akhareakshay <akhareakshay2@gmail.com>
2026-05-24 15:22:07 +0530

    initial commit

---
+++ src/main/java/com/codewithakshay/expense/tracker/ExpenseTrackerSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codewithakshay.expense.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTrackerSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithakshay/expense/tracker/ExpenseTrackerSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithakshay.expense.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
